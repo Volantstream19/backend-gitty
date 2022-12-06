7d0d5e605309c258818c4f676ef7ee1226eb9fdc
King Kong <king-kong@gmail.com>
2022-12-05 17:46:43 -0800

    C:/Program Files/Git/dashboard  return a user with no emaiil

---
+++ __tests__/github.test.js
@@ -0,0 +1,22 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/github');
+
+describe('user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('/api/v1/github/login should return redirect to github Oauth page', async () => {
+    const resp = await request(app).get('/api/v1/github/login');
+    expect(resp.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/callback/i
+    );
+  });
+});
